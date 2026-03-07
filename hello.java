16905ae2bb7619cd7f3aba6939e53dc65abf54da
Megha <Megha@gmail.com>
2026-03-07 11:38:36 +0530

    Added java file in javasample.java

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class hello {
     public static void main(String[] args) {
         // Print a message to the console
         System.out.println("Hello, World!");
