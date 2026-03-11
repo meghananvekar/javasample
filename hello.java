c49fe2b4819a0268b546063271eedf937302d2a1
meghananvekar <megha.bhat3@gmail.com>
2026-03-11 10:55:26 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class hello {
     public static void main(String[] args) {
         // Print a message to the console
         System.out.println("Hello, World!");
+        System.out.println("Hello, This is webhook!");
       System.out.println("This is Java code");
     }
 }
